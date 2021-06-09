031cfbfde1f8c6245af2e297d471cfe1c090e765
Takuya Murakami <tmurakam@tmurakam.org>
2021-06-09 21:32:00 +0900

    chore: update .gitignore

--- pypi-mirror.sh
+++ pypi-mirror.sh
@@ -2,7 +2,7 @@
 
 KUBESPRAY_DIR=${KUBESPRAY_DIR:-./kubespray}
 
-pypi-mirror download -d offline/pypi/files -r ${KUBESPRAY_DIR}/requirements.txt
-pypi-mirror create -d offline/pypi/files -m offline/pypi
+pypi-mirror download -d outputs/pypi/files -r ${KUBESPRAY_DIR}/requirements.txt
+pypi-mirror create -d outputs/pypi/files -m outputs/pypi
 
 
