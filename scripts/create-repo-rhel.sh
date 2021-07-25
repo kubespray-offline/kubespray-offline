c545eeab6f0372e369f568fae4a643d18403c759
Takuya Murakami <tmurakam@tmurakam.org>
2021-07-25 21:16:37 +0900

    fix: create repo rhel8 modules

--- scripts/create-repo-rhel.sh
+++ scripts/create-repo-rhel.sh
@@ -8,13 +8,24 @@ PKGS=$(cat pkglist/rhel/*.txt pkglist/rhel/${VERSION_ID}/*.txt | grep -v "^#" | 
 CACHEDIR=cache/cache-rpms
 mkdir -p $CACHEDIR
 
+IS_RHEL8=false
 if [ "$VERSION_ID" = "7" ]; then
     RT="sudo repotrack -a x86_64 -p $CACHEDIR"
 else
+    # RHEL 8
+    IS_RHEL8=true
     RT="sudo dnf download --resolve --alldeps --downloaddir $CACHEDIR"
+
+    # Install EPEL8
+    if ! command -v repo2module >/dev/null; then
+        echo "==> Install modulemd-tools"
+        sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+        sudo dnf copr enable -y frostyx/modulemd-tools-epel
+        sudo dnf install -y modulemd-tools
+    fi
 fi
 
-YD="sudo yumdownloader --destdir=$CACHEDIR -y"
+YD="yumdownloader --destdir=$CACHEDIR -y"
 
 echo "==> Downloading: " $PKGS
 $RT $PKGS || (echo "Download error" && exit 1)
@@ -28,10 +39,20 @@ mkdir -p $RPMDIR
 /bin/cp $CACHEDIR/*.rpm $RPMDIR/
 /bin/rm $RPMDIR/*.i686.rpm
 
-sudo createrepo $RPMDIR || exit 1
+echo "==> createrepo"
+createrepo $RPMDIR || exit 1
 
 #echo "==> Create repo tarball"
 #mkdir -p outputs/offline-files
 #(cd outputs && tar cvzf offline-files/offline-rpm-repo.tar.gz rpms)
+
+if $IS_RHEL8; then
+    cd $RPMDIR
+    #createrepo_c . || exit 1
+    echo "==> repo2module"
+    LANG=C repo2module -s stable . modules.yaml || exit 1
+    echo "==> modifyrepo"
+    modifyrepo_c --mdtype=modules modules.yaml repodata/ || exit 1
+fi
 
 echo "create-repo done."
