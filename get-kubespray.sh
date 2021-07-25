938f2055115bb0067e61a17ee6e6bf02635bc2cf
Takuya Murakami <tmurakam@tmurakam.org>
2021-07-25 13:30:50 +0900

    feat: add get-kubespray.sh

---
+++ get-kubespray.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KUBESPRAY_VERSION=2.16.0
+KUBESPRAY_TARBALL=kubespray-${KUBESPRAY_VERSION}.tar.gz
+
+if [ ! -d kubespray ]; then
+    if [ ! -e ${KUBESPRAY_TARBALL} ]; then
+        echo "===> Download ${KUBESPRAY_TARBALL}"
+        curl -SL https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v${KUBESPRAY_VERSION}.tar.gz >${KUBESPRAY_TARBALL} || exit 1
+    fi
+    echo "===> Extract ${KUBESPRAY_TARBALL}"
+    tar xzf ${KUBESPRAY_TARBALL}
+
+    mv kubespray-${KUBESPRAY_VERSION} kubespray
+fi
