75b3437d8779e4813cfbdd58c4e2bd85a0c5812e
Takuya Murakami <tmurakam@tmurakam.org>
2021-07-25 15:57:29 +0900

    fix: prepare.sh: rhel8

--- target-scripts/prepare.sh
+++ target-scripts/prepare.sh
@@ -5,16 +5,36 @@ if [ ! -d images ] && [ -d ../outputs ]; then
   BASEDIR="../outputs"  # for tests
 fi
 
+BASEDIR=$(cd $BASEDIR; pwd)
+
+cd $BASEDIR
+
 # Install docker-ce
 if ! command -v docker >/dev/null; then
   if [ -e /etc/redhat-release ]; then
-    :
+    cd $BASEDIR/rpms
+
+    # Install local yum repo
+    cat <<EOF | sudo tee /etc/yum.repos.d/local-repo.repo
+[local-repo]
+name=Local repo
+baseurl=file://$(pwd)
+enabled=0
+gpgcheck=0
+EOF
+
+    # ad hoc...
+    sudo yum localinstall -y --disablerepo="*" --enablerepo=local-repo container-selinux-* libslirp-* slirp4netns-* fuse-overlayfs-*
+
+    # Install docker-ce
+    sudo yum install -y --disablerepo="*" --enablerepo=local-repo docker-ce
   else
-    DEBDIR=$BASEDIR/debs/pkgs
-    sudo dpkg -i $DEBDIR/docker-ce*.deb $DEBDIR/containerd*.deb
+    cd $BASEDIR/debs/pkgs
+    sudo dpkg -i docker-ce*.deb containerd*.deb || exit 1
   fi
 fi
 
 # Load images
-sudo docker load -i $BASEDIR/images/docker.io-library-registry-*.tar
-sudo docker load -i $BASEDIR/images/docker.io-library-nginx-*.tar
+cd $BASEDIR/images
+sudo docker load -i ./images/docker.io-library-registry-*.tar
+sudo docker load -i ./images/docker.io-library-nginx-*.tar
