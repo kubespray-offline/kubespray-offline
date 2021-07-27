fe502a411a370136f0f9db6ea17e87559db75c7c
Takuya Murakami <tmurakam@tmurakam.org>
2021-07-27 11:54:04 +0900

    feat: add NGINX_PORT

--- target-scripts/start-nginx.sh
+++ target-scripts/start-nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./config.sh
+
 BASEDIR="."
 if [ ! -d images ] && [ -d ../outputs ]; then
   BASEDIR="../outputs"  # for tests
@@ -9,7 +11,7 @@ BASEDIR=$(cd $BASEDIR; pwd)
 NGINX_IMAGE=nginx:1.19
 
 sudo docker run -d \
-    -p 8080:80 \
+    -p ${NGINX_PORT}:80 \
     --restart always \
     --name nginx \
     -v ${BASEDIR}:/usr/share/nginx/html \
