6b2837dac169a96b0a2d9ffbcdb7ef28d0aabfb4
Takuya Murakami <tmurakam@tmurakam.org>
2021-07-30 19:30:08 +0900

    fix: travis tasks

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-./download-all.sh
+#!/bin/bash
+
+./prepare.sh || exit 1
+./get-kubespray.sh || exit 1
+#./pypi-mirror.sh || exit 1
+#./download-kubespray-files.sh || exit 1
+#./download-additional-containers.sh || exit 1
+./create-repo.sh || exit 1
+#./copy-target-scripts.sh || exit 1
+
+echo "Done."
