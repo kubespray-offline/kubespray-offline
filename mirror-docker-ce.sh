82a955773cdb6c7c98bcd2582c11ef5f3a67c914
Takuya Murakami <tmurakam@tmurakam.org>
2021-08-01 02:15:45 +0900

    feat: mirror-docker-ce: support rhel/centos

--- mirror-docker-ce.sh
+++ mirror-docker-ce.sh
@@ -5,10 +5,21 @@ source /etc/os-release
 /bin/rm -rf download.docker.com
 
 if [ -e /etc/redhat-release ]; then
-    :
+    if grep "Red Hat Enterprise Linux" /etc/redhat-release >/dev/null; then
+        type=rhel
+    else
+        type=centos
+    fi
+    
+    wget --mirror --no-parent \
+         https://download.docker.com/linux/${type}/${VERSION_ID}/x86_64/stable/   # need last slash for --no-parent
+
+    curl -SL https://download.docker.com/linux/${type}/gpg >download.docker.com/linux/${type}/gpg
+
+    /bin/rm -rf outputs/rpms/docker-ce
+    mv download.docker.com/linux/${type} outputs/rpms/docker-ce
 else
     # Ubuntu
-
     wget --mirror --no-parent \
          -X linux/ubuntu/dists/focal/pool/edge \
          -X linux/ubuntu/dists/focal/pool/nightly \
