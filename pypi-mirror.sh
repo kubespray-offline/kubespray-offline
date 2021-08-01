285f7d45ec6a97f80aa285978080c155f8354313
Takuya Murakami <tmurakam@tmurakam.org>
2021-08-01 17:24:09 +0900

    feat: download pypi binaries

--- pypi-mirror.sh
+++ pypi-mirror.sh
@@ -12,10 +12,13 @@ source ~/.venv/default/bin/activate
 export LANG=C  # It seems required for RHEL/CentOS?
 
 echo "==> Create pypi mirror for kubespray"
+set -x
 pip install -U pip
-pypi-mirror download -d outputs/pypi/files -r ${KUBESPRAY_DIR}/requirements.txt
-pypi-mirror download -d outputs/pypi/files pip setuptools wheel
-pypi-mirror download -d outputs/pypi/files pip setuptools==40.9.0 # For RHEL...
-pypi-mirror create -d outputs/pypi/files -m outputs/pypi
 
+OPTS="-d outputs/pypi/files"
+pypi-mirror download $OPTS -r ${KUBESPRAY_DIR}/requirements.txt || exit 1
+pypi-mirror download $OPTS --binary -r ${KUBESPRAY_DIR}/requirements.txt || exit 1
+pypi-mirror download $OPTS pip setuptools wheel || exit 1
+pypi-mirror download $OPTS pip setuptools==40.9.0 || exit 1  # For RHEL...
 
+pypi-mirror create -d outputs/pypi/files -m outputs/pypi || exit 1
