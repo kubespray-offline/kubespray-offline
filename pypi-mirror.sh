a5f895c697e1da1309571fe13934006c2d94d93b
Takuya Murakami <tmurakam@tmurakam.org>
2021-08-02 10:34:16 +0900

    feat: Download pypi binaries for all python versions

--- pypi-mirror.sh
+++ pypi-mirror.sh
@@ -12,13 +12,24 @@ source ~/.venv/default/bin/activate
 export LANG=C  # It seems required for RHEL/CentOS?
 
 echo "==> Create pypi mirror for kubespray"
-set -x
+#set -x
 pip install -U pip
 
 OPTS="-d outputs/pypi/files"
+echo "===> Download requirements"
 pypi-mirror download $OPTS -r ${KUBESPRAY_DIR}/requirements.txt || exit 1
-pypi-mirror download $OPTS --binary -r ${KUBESPRAY_DIR}/requirements.txt || exit 1
+
+sed "s/^ansible/#ansible/" ${KUBESPRAY_DIR}/requirements.txt > requirements2.txt
+for pyver in 3.6 3.7 3.8 3.9; do
+    echo "===> Download binary for python $pyver"
+    pypi-mirror download $OPTS --binary --python-version $pyver -r requirements2.txt || exit 1
+    pypi-mirror download $OPTS --binary --python-version $pyver PyYAML || exit 1
+done
+
+echo "===> Download pip, setuptools, wheel"
 pypi-mirror download $OPTS pip setuptools wheel || exit 1
 pypi-mirror download $OPTS pip setuptools==40.9.0 || exit 1  # For RHEL...
 
 pypi-mirror create -d outputs/pypi/files -m outputs/pypi || exit 1
+
+echo "pypi-mirror.sh done"
