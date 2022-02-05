b8383b6ab107cba2532df73167468eb17d8e28e1
Takuya Murakami <tmurakam@tmurakam.org>
2022-02-05 12:15:23 +0900

    fix: update docker files

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -3,6 +3,7 @@
 OPTS=""
 #OPTS="--progress=plain --no-cache=true"
 
-docker build -f Dockerfile.cent8 -t kubespray-offline-cent8:latest $OPTS ..
-
-docker build -f Dockerfile.ubuntu2004 -t kubespray-offline-ubuntu2004:latest $OPTS ..
+for i in alma8 ubuntu2004; do
+    echo "===> build $i"
+    docker build -f Dockerfile.$i -t kubespray-offline-$i:latest $OPTS ..
+done
--- docker/create-repo-in-docker.sh
+++ docker/create-repo-in-docker.sh
@@ -6,6 +6,6 @@ VOLUMES="-v ${ROOT}:${WORKDIR} -v /var/run/docker.sock:/var/run/docker.sock"
 
 CMD="cd ${WORKDIR} && ./create-repo.sh"
 
-docker run -it --rm ${VOLUMES} kubespray-offline-cent8:latest /bin/bash -c "${CMD}"
+docker run -it --rm ${VOLUMES} kubespray-offline-alma8:latest /bin/bash -c "${CMD}"
 
 docker run -it --rm ${VOLUMES} kubespray-offline-ubuntu2004:latest /bin/bash -c "${CMD}"
