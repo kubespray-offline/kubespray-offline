ecf6c69a538638685f073b094703cde6005b3c45
Takuya Murakami <murakami_da@nec.com>
2022-02-11 20:40:04 +0900

    fix: setup-container.sh: fix tarball name

--- target-scripts/setup-container.sh
+++ target-scripts/setup-container.sh
@@ -7,5 +7,5 @@
 echo "==> Load registry, nginx images"
 NERDCTL=/usr/local/bin/nerdctl
 cd ./images
-gunzip -c docker.io-library-registry-*.tar.gz | sudo $NERDCTL load
-gunzip -c docker.io-library-nginx-*.tar.gz | sudo $NERDCTL load
+gunzip -c docker.io_library_registry-*.tar.gz | sudo $NERDCTL load
+gunzip -c docker.io_library_nginx-*.tar.gz | sudo $NERDCTL load
