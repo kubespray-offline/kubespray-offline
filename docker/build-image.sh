2fd76693e94bca659f45e61e6e0c1020b72486ef
Takuya Murakami <tmurakam@tmurakam.org>
2022-02-28 23:09:08 +0900

    feat(docker): fix dockerfile

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -16,9 +16,9 @@ OPTS="--build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy"
 #OPTS="--progress=plain --no-cache=true"
 
 echo "===> build $target"
-docker build -f Dockerfile.$target -t tmurakam/kubespray-offline-$target:latest $OPTS ..
+docker build -f Dockerfile.$target -t tmurakam/kubespray-offline-$target:latest $OPTS .. || exit 1
 
-if [ $push ]; then
+if $push; then
     echo "===> push $target"
     docker push tmurakam/kubespray-offline-$target:latest
 fi
