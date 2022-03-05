b977dbddd5ede24b816dbec626323201a79547bb
Takuya Murakami <tmurakam@tmurakam.org>
2022-03-05 19:13:36 +0900

    chore(test): Add -s (skip) option to vagrant-test.sh

--- vagrant-test.sh
+++ vagrant-test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "usage: $0 <target>"
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <target> [-s]"
     exit 1
 fi
 target=$1
+if [ "$2" != "-s" ]; then
+    /bin/rm -rf cache/cache-* outputs/rpms output/debs
 
-/bin/rm -rf cache/cache-* outputs/rpms output/debs
-
-vagrant up $target || exit 1
-vagrant ssh $target -c "cd kubespray-offline && ./download-all.sh" || exit 1
-vagrant destroy -f $target || exit 1
+    vagrant up $target || exit 1
+    vagrant ssh $target -c "cd kubespray-offline && ./install-docker.sh && ./download-all.sh" || exit 1
+    vagrant destroy -f $target || exit 1
+else
+    echo "Skip build."
+fi
 
 vagrant up $target || exit 1
 vagrant ssh $target -c "cd kubespray-offline/test && ./test-install-offline.sh"
-
-
