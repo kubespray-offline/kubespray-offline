def435dd0a9462a73a28a386eca6150cb8b57243
Takuya Murakami <tmurakam@tmurakam.org>
2023-08-25 13:29:25 +0900

    feat(ci): change CI env: use both ubuntu 20.04 and 22.04

--- docker/download-files.sh
+++ docker/download-files.sh
@@ -5,4 +5,4 @@ source ./common.sh
 CMD="cd ${WORKDIR} && ./download-kubespray-files.sh"
 
 #docker run -it --rm -v ${ROOT}:${WORKDIR} kubespray-offline-cent8:latest /bin/bash -c "${CMD}"
-docker run -it --rm ${VOLUMES} tmurakam/kubespray-offline-ubuntu2004:latest /bin/bash -c "${CMD}"
+docker run -it --rm ${VOLUMES} tmurakam/kubespray-offline-ubuntu-22.04:latest /bin/bash -c "${CMD}"
