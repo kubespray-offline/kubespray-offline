c1d50639fa013618706112d8cad748153be43d0f
Takuya Murakami <tmurakam@tmurakam.org>
2024-01-04 14:29:43 +0900

    feat: update install-containerd.sh for kubespray 2.24 (#33)
    Align versions of runc, containerd, nerdctl

--- target-scripts/install-containerd.sh
+++ target-scripts/install-containerd.sh
@@ -29,10 +29,10 @@ download() {
 }
 
 if $ENABLE_DOWNLOAD; then
-    # TODO: These version must be same as kubespray. Refer `roles/downloads/defaults/main.yml` of kubespray.
-    RUNC_VERSION=1.1.9
-    CONTAINERD_VERSION=1.7.5
-    NERDCTL_VERSION=1.5.0
+    # TODO: These version must be same as kubespray. Refer `roles/kubespray-defaults/defaults/main/download.yml` of kubespray.
+    RUNC_VERSION=1.1.10
+    CONTAINERD_VERSION=1.7.11
+    NERDCTL_VERSION=1.7.1
     CNI_VERSION=1.3.0
 
     download https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.amd64 runc/v${RUNC_VERSION}
