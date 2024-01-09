6551ca43ce923f227501c4bb969e425c3a15d4b1
alexpirogovski <pirogovski@gmail.com>
2024-01-09 14:08:56 +0200

    - Use versioned registry (#25)
    - Use Kompton venv
    - No need to run igz_deploy with sudo

--- igz_files/igz_run_ansible.sh
+++ igz_files/igz_run_ansible.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-if [ -z "$docker" ]; then
-    docker=docker
-    if [ -e /usr/local/bin/nerdctl ]; then
-        docker=/usr/local/bin/nerdctl
-    fi
-fi
-
 # Make sure the container is not stuck from previous run
 docker rm -f kubespray_ansible 2>/dev/null || true
 
-$docker run -u $(id -u):$(id -g) \
+docker run -u root:root \
     -v "${PWD}":/work \
     -v ~/.ssh:/root/.ssh \
     -v /etc/ssh:/etc/ssh \
