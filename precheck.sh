6c91a53b2fd671c49d074e0fe51f944052678c58
Takuya Murakami <tmurakam@tmurakam.org>
2024-04-17 23:29:05 +0900

    fix: precheck.sh: don't check podman installation

--- precheck.sh
+++ precheck.sh
@@ -3,9 +3,11 @@
 source /etc/os-release
 source ./config.sh
 
-if ! command -v $docker >/dev/null 2>&1; then
-    echo "No $docker installed"
-    exit 1
+if [ "$docker" != "podman" ]; then
+    if ! command -v $docker >/dev/null 2>&1; then
+        echo "No $docker installed"
+        exit 1
+    fi
 fi
 
 if [ -e /etc/redhat-release ] && [[ "$VERSION_ID" =~ ^7.* ]]; then
