cd3d1d0f62a97a199a3904dbd62fcc0ddaee5b4c
Takuya Murakami <tmurakam@tmurakam.org>
2024-09-04 22:02:54 +0900

    feat: update target-scripts/config.sh for kubespray 2.26 (master)

--- target-scripts/config.sh
+++ target-scripts/config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Kubespray version to download. Use "master" for latest master branch.
-KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-2.25.0}
-#KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-master}
+#KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-2.26.0}
+KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-master}
 
 # These version must be same as kubespray.
 # Refer `roles/kubespray-defaults/defaults/main/download.yml` of kubespray.
-RUNC_VERSION=1.1.12
-CONTAINERD_VERSION=1.7.16
-NERDCTL_VERSION=1.7.4
-CNI_VERSION=1.3.0
+RUNC_VERSION=1.1.13
+CONTAINERD_VERSION=1.7.21
+NERDCTL_VERSION=1.7.6
+CNI_VERSION=1.4.0
 
 # container registry port
 REGISTRY_PORT=${REGISTRY_PORT:-35000}
