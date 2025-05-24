aa4d04c4fb37ab874d77755d5ff08ec8960c322d
Takuya Murakami <tmurakam@tmurakam.org>
2025-05-24 18:42:10 +0900

    feat: Use kubespray 2.28.0

--- target-scripts/config.sh
+++ target-scripts/config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Kubespray version to download. Use "master" for latest master branch.
-#KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-2.28.0}
-KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-master}
+KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-2.28.0}
+#KUBESPRAY_VERSION=${KUBESPRAY_VERSION:-master}
 
 # Versions of containerd related binaries used in `install-containerd.sh`
 # These version must be same as kubespray.
-# Refer `roles/kubespray-defaults/defaults/main/download.yml` of kubespray.
+# Refer `roles/kubespray_defaults/vars/main/checksums.yml` of kubespray.
 RUNC_VERSION=1.2.6
 CONTAINERD_VERSION=2.0.5
-NERDCTL_VERSION=2.0.4
+NERDCTL_VERSION=2.0.5
 CNI_VERSION=1.4.1
 
 # Some container versions, must be same as ../imagelists/images.txt
