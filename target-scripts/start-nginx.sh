bccd909311b34472fa42c1a096740fdd3bff8ae0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2026-02-16 18:03:56 +0900

    Use `--namespace default` explicitly

--- target-scripts/start-nginx.sh
+++ target-scripts/start-nginx.sh
@@ -7,7 +7,7 @@ if [ ! -d images ] && [ -d ../outputs ]; then
     BASEDIR="../outputs"  # for tests
 fi
 BASEDIR=$(cd $BASEDIR; pwd)
-NERDCTL="sudo /usr/local/bin/nerdctl"
+NERDCTL="sudo /usr/local/bin/nerdctl --namespace default"
 
 NGINX_IMAGE=nginx:${NGINX_VERSION}
 
--- target-scripts/start-registry.sh
+++ target-scripts/start-registry.sh
@@ -4,13 +4,19 @@ source ./config.sh
 
 REGISTRY_IMAGE=registry:${REGISTRY_VERSION}
 REGISTRY_DIR=${REGISTRY_DIR:-/var/lib/registry}
+NERDCTL="sudo /usr/local/bin/nerdctl --namespace default"
 
 if [ ! -e $REGISTRY_DIR ]; then
     sudo mkdir $REGISTRY_DIR
 fi
 
+echo "===> Stop registry"
+$NERDCTL container update --restart no registry 2>/dev/null
+$NERDCTL container stop registry 2>/dev/null
+$NERDCTL container rm registry 2>/dev/null
+
 echo "===> Start registry"
-sudo /usr/local/bin/nerdctl run -d \
+$NERDCTL run -d \
     --network host \
     -e REGISTRY_HTTP_ADDR=0.0.0.0:${REGISTRY_PORT} \
     --restart always \
