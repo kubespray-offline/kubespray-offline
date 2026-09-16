b18bd0bfc57260148f66525593f279bd5694c3c1
Takuya Murakami <takuya.murakami@gmail.com>
2026-09-16 16:35:53 +0900

    feat: Support Ubuntu 26.04 with Python 3.14

--- pypi-mirror.sh
+++ pypi-mirror.sh
@@ -30,7 +30,7 @@ cp ${KUBESPRAY_DIR}/requirements.txt $REQ
 echo "PyYAML" >> $REQ  # Ansible dependency
 echo "ruamel.yaml" >> $REQ # Inventory builder
 
-for pyver in 3.11 3.12; do
+for pyver in 3.11 3.12 3.14; do
     echo "===> Download binary for python $pyver"
     pip download $DEST --only-binary :all: --python-version $pyver $PLATFORM -r $REQ || exit 1
 done
